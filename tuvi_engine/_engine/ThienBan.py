c1dd54e88212381d1513ce7cf3c1c79675c1a13e
suneJQK <myloveforcfc96@gmail.com>
2026-08-20 00:33:59 +0700

    Update ThienBan.py from uploaded engine version

--- tuvi_engine/_engine/ThienBan.py
+++ tuvi_engine/_engine/ThienBan.py
@@ -1,15 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+(c) 2026 nmhaaa3218 <manh.ha.3218@gmail.com>
+"""
 import time
-from .AmDuong import canChiNgay,diaChi,ngayThangNam,ngayThangNamCanChi,nguHanh,nguHanhNapAm,sinhKhac,thienCan,timCuc
-class lapThienBan:
-    def __init__(self,nn,tt,nnnn,gioSinh,gioiTinh,ten,diaBan,duongLich=True,timeZone=7):
-        self.gioiTinh=1 if gioiTinh==1 else -1; self.namNu='Nam' if gioiTinh==1 else 'Nữ'; self.timeZone=timeZone
-        self.chiGioSinh=diaChi[gioSinh]; self.canGioSinh=((int((nnnn+6)%10)+gioSinh-1)%10)+1; self.gioSinh='{} {}'.format(thienCan[self.canGioSinh]['tenCan'],self.chiGioSinh['tenChi'])
-        self.ngayDuong,self.thangDuong,self.namDuong,self.ten=nn,tt,nnnn,ten
-        if duongLich: self.ngayAm,self.thangAm,self.namAm,self.thangNhuan=ngayThangNam(nn,tt,nnnn,True,timeZone)
-        else: self.ngayAm,self.thangAm,self.namAm=nn,tt,nnnn
-        self.canThang,self.canNam,self.chiNam=ngayThangNamCanChi(self.ngayAm,self.thangAm,self.namAm,False,timeZone); self.chiThang=self.thangAm
-        self.canThangTen=thienCan[self.canThang]['tenCan']; self.canNamTen=thienCan[self.canNam]['tenCan']; self.chiThangTen=diaChi[(self.thangAm+1)%12+1]['tenChi']; self.chiNamTen=diaChi[self.chiNam]['tenChi']
-        self.canNgay,self.chiNgay=canChiNgay(nn,tt,nnnn,duongLich,timeZone); self.canNgayTen=thienCan[self.canNgay]['tenCan']; self.chiNgayTen=diaChi[self.chiNgay]['tenChi']
-        cungAD=1 if diaBan.cungMenh%2 else -1; namAD=1 if self.chiNam%2 else -1; self.amDuongNamSinh='Dương' if namAD==1 else 'Âm'; self.amDuongMenh='Âm dương thuận lý' if cungAD*namAD==1 else 'Âm dương nghịch lý'
-        cuc=timCuc(diaBan.cungMenh,self.canNam); self.hanhCuc=nguHanh(cuc)['id']; self.tenCuc=nguHanh(cuc)['tenCuc']; self.menhChu=diaChi[diaBan.cungMenh]['menhChu']; self.thanChu=diaChi[self.chiNam]['thanChu']; self.menh=nguHanhNapAm(self.chiNam,self.canNam); self.banMenh=nguHanhNapAm(self.chiNam,self.canNam,True)
-        self.sinhKhac='Mệnh Cục bình hòa'
+
+from .._lunar_calendar.util.VnCalendarUtil import jdFromDate
+from .AmDuong import (
+    canChiNgay,
+    diaChi,
+    ngayThangNam,
+    ngayThangNamCanChi,
+    nguHanh,
+    nguHanhNapAm,
+    sinhKhac,
+    thienCan,
+    timCuc,
+)
+
+
+class lapThienBan(object):
+    def __init__(self, nn, tt, nnnn, gioSinh, gioiTinh, ten, diaBan,
+                 duongLich=True, timeZone=7):
+        super(lapThienBan, self).__init__()
+        self.gioiTinh = 1 if gioiTinh == 1 else -1
+        self.namNu = "Nam" if gioiTinh == 1 else "Nữ"
+
+        chiGioSinh = diaChi[gioSinh]
+        if duongLich is False:
+            from .Lich_HND import L2S
+            s_date = L2S(nn, tt, nnnn, 0, timeZone)
+            jd = jdFromDate(s_date[0], s_date[1], s_date[2])
+        else:
+            jd = jdFromDate(nn, tt, nnnn)
+        canGioSinh = ((jd - 1) * 2 % 10 + gioSinh) % 10
+        if canGioSinh == 0:
+            canGioSinh = 10
+        self.chiGioSinh = chiGioSinh
+        self.canGioSinh = canGioSinh
+        self.gioSinh = "{} {}".format(thienCan[canGioSinh]['tenCan'],
+                                       chiGioSinh['tenChi'])
+
+        self.timeZone = timeZone
+        self.today = time.strftime("%d/%m/%Y")
+        self.ngayDuong, self.thangDuong, self.namDuong, self.ten = \
+            nn, tt, nnnn, ten
+        if duongLich is True:
+            self.ngayAm, self.thangAm, self.namAm, self.thangNhuan = \
+                ngayThangNam(self.ngayDuong, self.thangDuong, self.namDuong,
+                             True, self.timeZone)
+        else:
+            self.ngayAm, self.thangAm, self.namAm = self.ngayDuong, \
+                self.thangDuong, self.namDuong
+
+        self.canThang, self.canNam, self.chiNam = \
+            ngayThangNamCanChi(self.ngayAm, self.thangAm,
+                               self.namAm, False, self.timeZone)
+        self.chiThang = self.thangAm
+        self.canThangTen = thienCan[self.canThang]['tenCan']
+        self.canNamTen = thienCan[self.canNam]['tenCan']
+        self.chiThangTen = diaChi[(self.thangAm + 1) % 12 + 1]['tenChi']
+        self.chiNamTen = diaChi[self.chiNam]['tenChi']
+
+        self.canNgay, self.chiNgay = canChiNgay(
+            self.ngayDuong, self.thangDuong, self.namDuong,
+            duongLich, timeZone)
+        self.canNgayTen = thienCan[self.canNgay]['tenCan']
+        self.chiNgayTen = diaChi[self.chiNgay]['tenChi']
+
+        cungAmDuong = 1 if (diaBan.cungMenh % 2 == 1) else -1
+        namSinhAmDuong = 1 if (self.chiNam % 2 == 1) else -1
+        self.amDuongNamSinh = "Dương" if (self.chiNam % 2 == 1) else "Âm"
+        if (cungAmDuong * namSinhAmDuong == 1):
+            self.amDuongMenh = "Âm dương thuận lý"
+        else:
+            self.amDuongMenh = "Âm dương nghịch lý"
+
+        cuc = timCuc(diaBan.cungMenh, self.canNam)
+        self.hanhCuc = nguHanh(cuc)['id']
+        self.tenCuc = nguHanh(cuc)['tenCuc']
+
+        self.menhChu = diaChi[diaBan.cungMenh]['menhChu']
+        self.thanChu = diaChi[self.chiNam]['thanChu']
+
+        self.menh = nguHanhNapAm(self.chiNam, self.canNam)
+        menhId = nguHanh(self.menh)['id']
+        menhCuc = sinhKhac(menhId, self.hanhCuc)
+        if menhCuc == 1:
+            self.sinhKhac = "Mệnh sinh Cục"
+        elif menhCuc == -1:
+            self.sinhKhac = "Mệnh khắc Cục"
+        elif menhCuc == -1j:
+            self.sinhKhac = "Cục khắc Mệnh"
+        elif menhCuc == 1j:
+            self.sinhKhac = "Cục sinh Mệnh"
+        else:
+            self.sinhKhac = "Mệnh Cục bình hòa"
+
+        self.banMenh = nguHanhNapAm(self.chiNam, self.canNam, True)
