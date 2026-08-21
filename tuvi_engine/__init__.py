47b8bc975c8c7fa7c4d9bb5d0e9a1b0b58bc4630
suneJQK <myloveforcfc96@gmail.com>
2026-08-22 00:34:15 +0700

    fix: wire source-accurate Tiểu vận mapping

--- tuvi_engine/__init__.py
+++ tuvi_engine/__init__.py
@@ -8,6 +8,18 @@ from .ai_context import build_ai_context, load_relationship_knowledge
 from .engine import cached_lap_la_so, clear_chart_cache, lap_la_so
 from .schema import SCHEMA_VERSION, build_meta, to_v2_chart, validate_v2_chart
 
+# Patch Tiểu vận trước khi bất kỳ caller nào lấy calculate_van_layers.
+# Module này port đúng thứ tự rl=check(rl) của mã nguồn tham khảo.
+from . import van_calculator as _van_calculator
+from .van_tieu_van_patch import build_tieu_van_source_mapping
+_van_calculator._tieu_van_source_mapping = lambda birth_branch, target_branch, gender: build_tieu_van_source_mapping(
+    _van_calculator.check,
+    _van_calculator.chi_name,
+    birth_branch,
+    target_branch,
+    gender,
+)
+
 __all__ = [
     "lap_la_so",
     "cached_lap_la_so",
